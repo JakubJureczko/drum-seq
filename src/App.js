055db8fabe026ef0cae5af55eb7c37f971d0d3ec
Jakub Jureczko <untech.untech@gmail.com>
2020-09-09 00:41:54 +0200

    start button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Sampler } from "tone";
-import styled from 'styled-components';
-import Sequencer from './Seq'
-
+import styled from "styled-components";
+import Sequencer from "./Seq";
 
 import A1 from "./assets/S1.wav";
 import A2 from "./assets/S2.wav";
@@ -10,19 +9,18 @@ import A3 from "./assets/S3.wav";
 import A4 from "./assets/S6.wav";
 import A5 from "./assets/S7.wav";
 
-
-import './App.css'
-import DrumPads from './Pady';
+import "./App.css";
+import DrumPads from "./Pady";
 
 function App() {
   const [isLoaded, setLoaded] = useState(false);
   const sampler = useRef(null);
-  const [currentSampler, setCurrentSampler] = useState()
-  
-  const[color, setColor] = useState('');
+  const [currentSampler, setCurrentSampler] = useState();
+
+  const [color, setColor] = useState("");
 
   const Title = styled.button`
-  background-color: green;
+    background-color: green;
   `;
 
   useEffect(() => {
@@ -30,81 +28,72 @@ function App() {
       { A1, A2, A3, A4, A5 },
       {
         onload: () => {
-          
-            setLoaded(true);}
+          setLoaded(true);
         },
-      
+      }
     ).toDestination();
   }, []);
 
   const handleClick = () => sampler.current.triggerAttack("A1");
-  
+
   const handleClick2 = () => {
-    if(currentSampler) {
-      currentSampler.stop()
+    if (currentSampler) {
+      currentSampler.stop();
     }
     setCurrentSampler(sampler.current);
-    sampler.current.triggerAttack("A2")
-  }
-  
+    sampler.current.triggerAttack("A2");
+  };
+
   const handleClick3 = () => sampler.current.triggerAttack("A3");
-  
+
   const handleClick4 = () => sampler.current.triggerAttack("A4");
   const handleClick5 = () => sampler.current.triggerAttack("A5");
   const handleClickX = () => sampler.current.triggerAttack("");
 
   const changeColorsssss = (e) => {
-     styled.button`
-    background-color: green;
+    styled.button`
+      background-color: green;
     `;
-  }
+  };
 
   // useEffect(() =>
   //   document.addEventListener("keydown", (e) => {
   //     handleKeyPress(e.code)
-     
-      
+
   //   })
 
   // );
   useEffect(() => {
     document.addEventListener("keydown", (e) => {
-        handleKeyPress(e.code)
-    })
-    
-    document.addEventListener("keyup", (e) => {
-      if(currentSampler){
-        currentSampler.stop()
-      }
-      
+      handleKeyPress(e.code);
     });
-         
-       
 
-  },[]);
+    document.addEventListener("keyup", (e) => {
+      if (currentSampler) {
+        currentSampler.stop();
+      }
+    });
+  }, []);
 
+  function changeColor(color) {
+    document.button.style.background = color;
+  }
+  const btn = document.getElementById("btn");
 
-  function changeColor(color) { 
-    document.button.style.background = color; 
-} 
-  const btn = document.getElementById("btn")
-
-function colorFunction() { 
-    changeColor('yellow'); 
-    btn.innerHTML = ""; 
-}     
-  
+  function colorFunction() {
+    changeColor("yellow");
+    btn.innerHTML = "";
+  }
 
   function handleKeyPress(keyCode) {
-    if(currentSampler){
-      currentSampler.stop()
+    if (currentSampler) {
+      currentSampler.stop();
     }
 
     switch (keyCode) {
-      case "KeyD": 
-     
-      handleClick()
-      
+      case "KeyD":
+        handleClick();
+
         break;
       case "KeyF":
         handleClick2();
@@ -123,18 +112,16 @@ function colorFunction() {
     }
   }
 
-
   return (
     <div className="app">
-      <button id="btn" disabled={!isLoaded}   
-        onClick={handleClick}> 
+      <button id="btn" disabled={!isLoaded} onClick={handleClick}>
         BD/D
       </button>
-     
-      <button id="btn" disabled={!isLoaded} onClick={handleClick2} >
+
+      <button id="btn" disabled={!isLoaded} onClick={handleClick2}>
         SD/F
       </button>
-      <button  id="btn" disabled={!isLoaded} onClick={handleClick4}>
+      <button id="btn" disabled={!isLoaded} onClick={handleClick4}>
         CH/J
       </button>
       <button id="btn" disabled={!isLoaded} onClick={handleClick5}>
@@ -143,9 +130,9 @@ function colorFunction() {
       <button id="btn" disabled={!isLoaded} onClick={handleClick3}>
         SMPL/L
       </button>
-      < Sequencer />
+      <Sequencer />
     </div>
   );
-};
+}
 
 export default App;
--- src/Seq.js
+++ src/Seq.js
@@ -1,6 +1,14 @@
-import React, { useCallback, useState, useEffect } from "react"
-import * as Tone from "tone"
-import  "./Seq.css"
+import React, { useCallback, useState, useEffect } from "react";
+import * as Tone from "tone";
+import "./Seq.css";
+
+import {
+  faPlay,
+  faStop,
+  faRecordVinyl,
+  faCompactDisc,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import D1 from "./assets/bd1.mp3";
 import D2 from "./assets/bd2.mp3";
@@ -10,101 +18,125 @@ import D5 from "./assets/ch2.mp3";
 import D6 from "./assets/ch2.mp3";
 import D7 from "./assets/oh.mp3";
 
-const drums = [
-  "D1",
-  "D2",
-  "D3",
-  "D4",
-  "D5",
-  "D6",
-  'D7'
-]
+const drums = ["D1", "D2", "D3", "D4", "D5", "D6", "D7"];
 
 const initialPattern = [
-  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
 const clickStyle = (color) => {
   document.getElementById("sq").style.background = color;
-   
+};
 
-}
+// Tone.Transport.state
 
 const seq = new Tone.Sampler({
-   D1, D2,D3, D4, D5, D6, D7
-}).toMaster()
+  D1,
+  D2,
+  D3,
+  D4,
+  D5,
+  D6,
+  D7,
+}).toMaster();
 
 const Sequencer = () => {
-  
-  const [ playState, setPlayState ] = useState(false)
-  const [ activeColumn, setColumn ] = useState(0)
-  const [ pattern, updatePattern ] = useState(initialPattern)
+  const [playState, setPlayState] = useState(false);
+  const [activeColumn, setColumn] = useState(0);
+  const [pattern, updatePattern] = useState(initialPattern);
 
   useEffect(
     () => {
       const loop = new Tone.Sequence(
         (time, col) => {
           // Update active column for animation
-          setColumn(col)
+          setColumn(col);
 
           // Loop current pattern
           pattern.map((row, noteIndex) => {
             // If active
             if (row[col]) {
               // Play based on which row
-              seq.triggerAttackRelease(drums[noteIndex], "8n", time)
+              seq.triggerAttackRelease(drums[noteIndex], "8n", time);
             }
-          })
+          });
         },
-        [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         "16n"
-      ).start(0)
-      return () => loop.dispose()
+      ).start(0);
+      return () => loop.dispose();
     },
-    [ pattern ] // Retrigger when pattern changes
-  )
+    [pattern] // Retrigger when pattern changes
+  );
+
+  function handleStart() {
+    setPlayState(!playState);
+  }
 
   // Toggle playing / stopped
   const toggle = useCallback(() => {
-    Tone.Transport.toggle()
-    setPlayState(!playState)
-  }, [])
+    Tone.Transport.toggle();
+  }, []);
 
   // Update pattern by making a copy and inverting the value
   function setPattern({ x, y, value }) {
-    const patternCopy = [ ...pattern ]
-    patternCopy[y][x] = +!value
-    updatePattern(patternCopy)
+    const patternCopy = [...pattern];
+    patternCopy[y][x] = +!value;
+    updatePattern(patternCopy);
   }
   return (
     <div>
       {pattern.map((row, y) => (
         <div className="outter">
-        <div key={y} style={{ display: "flex", justifyContent: "center" }}>
-          {row.map((value, x) => (
-            <Square
-              key={x}
-              active={activeColumn === x}
-              selected={value}
-              onClick={() => setPattern({ x, y, value })}
-            />
-          ))}
-        </div>
+          <div key={y} style={{ display: "flex", justifyContent: "center" }}>
+            {row.map((value, x) => (
+              <Square
+                key={x}
+                active={activeColumn === x}
+                selected={value}
+                onClick={() => setPattern({ x, y, value })}
+              />
+            ))}
+          </div>
         </div>
       ))}
-      <button onClick={() => toggle()}>{playState ? "on" : "off"}</button>
+      <button
+        className="startBtn"
+        onClick={() => {
+          toggle();
+          handleStart();
+        }}
+      >
+        {playState ? (
+          <FontAwesomeIcon
+            icon={faCompactDisc}
+            size="8x"
+            style={{ color: "rgba(133, 65, 243, 0.8)" }}
+            transform="left-1"
+            spin
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faRecordVinyl}
+            size="8x"
+            style={{ color: "rgba(133, 65, 243, 0.8)" }}
+            transform="left-1.1"
+          />
+        )}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 const Square = ({ active, value, onClick }) => (
-  <div id="sq"
+  <div
+    id="sq"
     style={{
       display: "flex",
       alignItems: "center",
@@ -112,17 +144,13 @@ const Square = ({ active, value, onClick }) => (
       borderRadius: active ? "" : "10%",
       width: 30,
       height: 30,
-      background: value ? "pink" : "blue",
       border: active ? "1px solid rgba(133, 65, 243, 0.1)" : "1px solid #eee",
       background: active ? "rgba(133, 65, 243, 0.3)" : "",
-      
-      
     }}
     onClick={onClick}
-  
   >
     {value}
   </div>
-)
+);
 
 export default Sequencer;
