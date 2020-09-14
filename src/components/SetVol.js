7816907a1b456e7ffbbf975dac89dda86b1834b5
Jakub Jureczko <untech.untech@gmail.com>
2020-09-14 14:00:06 +0200

    knobs

--- src/components/SetVol.js
+++ src/components/SetVol.js
@@ -2,36 +2,53 @@ import React, { useState } from "react";
 import * as Tone from "tone";
 import "./SetVol.css";
 
+import Knob from "react-simple-knob";
+
 const SetVol = () => {
-  const [vol, setVol] = useState(0)
+  const [vol, setVol] = useState(0);
   Tone.Master.volume.value = vol;
 
   function volUp() {
-    if(vol < 6) {
-      setVol(vol + 1)
-    }else {
-      setVol(vol)
+    if (vol < 6) {
+      setVol(vol + 1);
+    } else {
+      setVol(vol);
     }
   }
-  function volDown(){
-    if(vol > -30) {
-      setVol(vol - 1)
-    }else {
-      setVol(vol)
+  function volDown() {
+    if (vol > -30) {
+      setVol(vol - 1);
+    } else {
+      setVol(vol);
     }
   }
 
-  return(
-   <div className="setvol">
-        <span>{vol} dB</span>
-        <button onMouseDown={volUp}>
-        +
-        </button>
-        <button onMouseDown={volDown}>-</button>
-        
-        
+  const style = {
+    height: "4em",
+    fontFamily: "Arial",
+    color: "black",
+  };
+
+  return (
+    <div className="setvol">
+      <span>{vol} dB</span>
+      <button onMouseDown={volUp}>+</button>
+      <button onMouseDown={volDown}>-</button>
+      <div>
+        <Knob
+          name="Vol"
+          unit="dB"
+          defaultPercentage={vol}
+          onChange={setVol}
+          bg="black"
+          fg="white"
+          mouseSpeed={1}
+          transform={(p) => parseInt(p * 30, 10) - 30}
+          style={style}
+        />
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default SetVol;
