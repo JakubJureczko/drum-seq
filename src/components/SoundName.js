e63bf1819764ef7c7335f79744d2a2140996fe22
Jakub Jureczko <untech.untech@gmail.com>
2020-09-18 18:57:16 +0200

    soundname css

--- src/components/SoundName.js
+++ src/components/SoundName.js
@@ -1,6 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react'
 import {Sampler} from "tone";
 import "./SoundName.css"
+import classnames from "classnames";
 
 import D1 from "../assets/drums/bd1.mp3";
 import D2 from "../assets/drums/bd2.mp3";
@@ -12,33 +13,34 @@ import D7 from "../assets/drums/oh.mp3";
 
 
 function SoundName(){
-  const [isLoaded, setLoaded] = useState(false);
   
+  const [isLoaded, setLoaded] = useState(false);
   const sampler = useRef(null);
+  const [currentSampler, setCurrentSampler] = useState();
 
   useEffect(() => {
-    sampler.current = new Sampler( {
-      onload: () => {
-        setLoaded(true);
-      },
-    }).toDestination();
+    sampler.current = new Sampler(
+      { D1, D2, D3, D4, D5, D6, D7 },
+      {
+        onload: () => {
+          setLoaded(true);
+        },
+      }
+    ).toDestination();
   }, []);
 
-  const play = (sound) => {
-   
-    
-    sampler.current.triggerAttack(sound);
-  };
+  const handleClick = (sound) => sampler.current.triggerAttack(sound);
+  
 
   return (
     <div className="soundname"> 
-    <span onMouseDown={() => {play(D1)}}className="bass1"></span>
-    <span className="bass2"></span>
-    <span className="snare"></span>
-    <span className="hihat"></span>
-    <span className="openhat"></span>
-    <span className="openhat2"></span>
-    <span className="closehat"></span>
+    <span onMouseDown={() => {handleClick("D1")}} className="bass1"></span>
+    <span onMouseDown={() => {handleClick("D2")}} className="bass2"></span>
+    <span onMouseDown={() => {handleClick("D3")}} className="snare"></span>
+    <span onMouseDown={() => {handleClick("D4")}} className="hihat"></span>
+    <span onMouseDown={() => {handleClick("D5")}} className="hihat2"></span>
+    <span onMouseDown={() => {handleClick("D6")}} className="openhat"></span>
+    <span onMouseDown={() => {handleClick("D7")}} className="closehat"></span>
     </div>
   )
 }
