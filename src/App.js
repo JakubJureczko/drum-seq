4362f53ca5cd5dbe148231dcb6bee75de2b6a9a7
Jakub Jureczko <untech.untech@gmail.com>
2020-09-19 00:42:38 +0200

    clear button function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import styled from "styled-components";
 import Sequencer from "./components/Seq";
 import SampleBtn from "./components/SampleBtn"
 import Actions from "./components/Actions"
-
 import SetBpm from "./components/SetBpm";
 import SetVol from "./components/SetVol";
 import Swing from "./components/Swing";
+import Clear from "./components/Clear"
 
 import "./App.css";
 
