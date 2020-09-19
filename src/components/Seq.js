60130622974cc5fcf47e3bf60c780f08a493c02a
Jakub Jureczko <untech.untech@gmail.com>
2020-09-19 15:05:39 +0200

    new border

--- src/components/Seq.js
+++ src/components/Seq.js
@@ -149,7 +149,7 @@ const Square = ({ active, row, selected, onClick, col }) => {
         width: "2em",
         height: "2em",
         margin: "0.1em",
-        border: active ? "2px solid rgb(167, 167, 167)" : "2px solid #eee", //`2px solid ${getColumnColor(key)}`,   //"2px solid #eee"
+        border: active ? "2px solid rgb(167, 167, 167)" : "2px solid rgba(167, 167, 167, 0.3)", //`2px solid ${getColumnColor(key)}`,   //"2px solid #eee"
         //background: active ? "rgba(133, 65, 243, 0.9)" : "",
         background: selected ? getColor(row) : getColumnColor(col),
         backgroundColor: getColumnColor(col),
