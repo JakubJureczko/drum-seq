e8a64818aa8568553d51cca9f835a8af09c40d26
Jakub Jureczko <untech.untech@gmail.com>
2020-09-25 11:53:16 +0200

    recorder btns

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -1,42 +1,52 @@
-import { render } from '@testing-library/react'
-import React from "react"
-import *as Tone from "tone"
-import ReactAudioPlayer from 'react-audio-player';
+import { render } from "@testing-library/react";
+import React, { useState } from "react";
+import * as Tone from "tone";
+import ReactAudioPlayer from "react-audio-player";
+
+import "./Recorder.css"
+
+import recon from "../assets/images/recordon.svg";
+import recoff from "../assets/images/record.svg";
 
 const Recorder = () => {
-  
+  const [rec, setRec] = useState(false);
+
   const recorder = new Tone.Recorder();
   Tone.Master.connect(recorder);
- 
+
   recorder.start();
- 
- 
-  
-  setTimeout(async () => {  
+
+  setTimeout(async () => {
     const recording = await recorder.stop();
     const url = URL.createObjectURL(recording);
     const anchor = document.createElement("a");
     anchor.download = "beat.wav";
     anchor.href = url;
     anchor.click();
-  }, 10000); 
+  }, 10000);
 
-//   document.body.appendChild(anchor);
-// anchor.click();
-// document.body.removeChild(anchor);
+  //   document.body.appendChild(anchor);
+  // anchor.click();
+  // document.body.removeChild(anchor);
 
-  return(
-    <div>
-   <a href={recorder} target="_blank"  download>
-   <button>
-      
-      Download File
-   </button>
-</a> 
+  return (
+    <div className="recordbtn">
+      <div className="start">
+        <button onMouseDown={() => setRec(!rec)}>
+          <img
+            src={rec ? recon : recoff}
+            alt="record"
+           
+          />
+        </button>
+      </div>
+      <div className="download">
+      <a href={recorder} target="_blank" download>
+        <button></button>
+      </a>
+      </div>
     </div>
-  )
-
-  
-}
+  );
+};
 
 export default Recorder;
