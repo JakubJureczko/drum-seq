f5abe1ec9688af7f0e27ff2b362e24c5ec906423
Jakub Jureczko <untech.untech@gmail.com>
2020-09-28 12:03:12 +0200

    set vol

--- src/components/SetVol.js
+++ src/components/SetVol.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
-import * as Tone from "tone";
-import "./SetVol.css";
+import React, {useContext, useRef, useEffect,useState} from 'react';
 
-//import Knob from "react-simple-knob";
+import {VolumeContext} from "../volumeContext"
 
 function useInterval(callback, delay) {
   const savedCallback = useRef();
@@ -24,38 +22,32 @@ function useInterval(callback, delay) {
 
 
 
-const SetVol = () => {
-  const [vol, setVol] = useState(0);
+const SeqVol = () => {
+  const {vol,setVol} = useContext(VolumeContext)
   const [isMousePlus, setMousePlus] = useState(false);
   const [isMouseMinus, setMouseMinus] = useState(false);
+
   
-  Tone.Master.volume.value = vol;
 
   function volUp() {
     if (vol < 6) {
-      setVol(vol + 1);
+      setVol((prevVol) => prevVol + 1);
     } else {
-      setVol(vol);
+      setVol((prevVol) => prevVol );
     }
   }
   function volDown() {
     if (vol > -30) {
-      setVol(vol - 1);
+      setVol((prevVol) => prevVol - 1);
     } else {
-      setVol(vol);
+      setVol((prevVol) => prevVol);
     }
   }
   useInterval(volUp, isMousePlus ? 150 : null);
   useInterval(volDown, isMouseMinus ? 150 : null);
 
-  // const style = {
-  //   height: "4em",
-  //   fontFamily: "Arial",
-  //   color: "black",
-  // };
-
   return (
-    <div className="setVol">
+<div className="setVol">
       <div className='spanVol'>
         <span>{vol}</span>
         <span className="dB">dB</span>  
@@ -63,42 +55,22 @@ const SetVol = () => {
       <div className="setVolBtn">
       <button onMouseDown={() => {
         setMousePlus(true);
-        if (vol < 6) {
-          setVol(vol + 1);
-        } else {
-          setVol(vol);
-        }
+        volUp();
         }}
         onMouseUp={() => setMousePlus(false)}
         onMouseLeave={() => setMousePlus(false)}
         >+</button>
       <button onMouseDown={() => {
         setMouseMinus(true);
-        if (vol > -30) {
-          setVol(vol - 1);
-        } else {
-          setVol(vol);
-        }
+        volDown();
         }}
         onMouseUp={() => setMouseMinus(false)}
         onMouseLeave={() => setMouseMinus(false)}
         >-</button>
       </div>
-      {/* <div>
-        <Knob
-          name="Vol"
-          unit="dB"
-          defaultPercentage={vol}
-          onChange={setVol}
-          bg="black"
-          fg="white"
-          mouseSpeed={1}
-          transform={(p) => parseInt(p * 30, 10) - 30}
-          style={style}
-        />
-      </div> */}
+      
     </div>
-  );
-};
+  )
+}
 
-export default SetVol;
+export default SeqVol;
