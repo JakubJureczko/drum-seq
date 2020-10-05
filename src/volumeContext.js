b437b4ddee0bdd973d78bc8731f074c559ae30f1
Jakub Jureczko <untech.untech@gmail.com>
2020-10-05 14:30:56 +0200

    volume context

--- src/volumeContext.js
+++ src/volumeContext.js
@@ -1,5 +1,5 @@
 import React, {createContext, useState, useRef,} from "react"
-
+import triggers from "./components/triggers"
 
 
 export const VolumeContext = createContext({});
@@ -7,11 +7,16 @@ export const VolumeContext = createContext({});
 const VolumeContextProvider = ({ children }) => {
   const [vol, setVol] = useState(0);
 
-  
+  const sounds = triggers.reduce((acc, { sound, name }) => {
+    return {
+      ...acc,
+      [name]: sound,
+    };
+  }, {});
     
 
 return (
-  <VolumeContext.Provider value={{vol, setVol}}>
+  <VolumeContext.Provider value={{vol, setVol, sounds}}>
     {children}
   </VolumeContext.Provider>
 )
