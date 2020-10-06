29a6fc44b7aec5da5d450004a9c7156adf3a2250
Jakub Jureczko <untech.untech@gmail.com>
2020-10-06 17:04:12 +0200

    button colors on change

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,11 +4,13 @@ import { Sampler } from "tone";
 import ReactDOM from "react-dom";
 import "./Modal.css";
 import { VolumeContext } from "../volumeContext";
-import { defaultSounds } from './triggers';
+import { defaultSounds } from "./triggers";
 
 const Modal = ({ isShowing, hide }) => {
-
   const { sounds, setSounds } = useContext(VolumeContext);
+
+  const [uploads, setUploads] = useState([]);
+  const [color, setColor] = useState("");
 
   // const db = new Dexie("ReactDexie");
   // //create the database store
@@ -31,10 +33,15 @@ const Modal = ({ isShowing, hide }) => {
     reader.readAsDataURL(e[0]);
     reader.onload = (e) => {
       setFile(reader.result);
-      setSounds({...sounds, [name]: reader.result})
+      setSounds({ ...sounds, [name]: reader.result });
+      setUploads([name, ...uploads]);
     };
   };
 
+  function setDefault() {
+    setSounds(defaultSounds);
+    setUploads([]);
+  }
   // const [isLoaded, setLoaded] = useState(false);
   // const sampler = useRef(null);
 
@@ -61,7 +68,7 @@ const Modal = ({ isShowing, hide }) => {
   //   getPosts();
   // }, []);
 
-  
+  console.log(uploads);
 
   return isShowing
     ? ReactDOM.createPortal(
@@ -77,15 +84,24 @@ const Modal = ({ isShowing, hide }) => {
               </button> */}
               </div>
               <div className="modalbtn">
-                <label className="fileupload">
+                <label
+                  className="fileupload"
+                  style={{
+                    backgroundColor: uploads.indexOf("A1") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}
+                >
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A1", e.target.files)}
+                    onChange={(e) => {
+                      getFile("A1", e.target.files);
+                    }}
                   />
                   E
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A2") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
@@ -93,91 +109,112 @@ const Modal = ({ isShowing, hide }) => {
                   />
                   R
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A3") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A3",e.target.files)}
+                    onChange={(e) => getFile("A3", e.target.files)}
                   />
                   T
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A4") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A4",e.target.files)}
+                    onChange={(e) => getFile("A4", e.target.files)}
                   />
                   U
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A5") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A5",e.target.files)}
+                    onChange={(e) => getFile("A5", e.target.files)}
                   />
                   I
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A6") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A6",e.target.files)}
+                    onChange={(e) => getFile("A6", e.target.files)}
                   />
                   O
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A7") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A7",e.target.files)}
+                    onChange={(e) => getFile("A7", e.target.files)}
                   />
                   D
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A8") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A8",e.target.files)}
+                    onChange={(e) => getFile("A8", e.target.files)}
                   />
                   F
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A9") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A9",e.target.files)}
+                    onChange={(e) => getFile("A9", e.target.files)}
                   />
                   G
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A10") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A10",e.target.files)}
+                    onChange={(e) => getFile("A10", e.target.files)}
                   />
                   J
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A11") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}>
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A11",e.target.files)}
+                    onChange={(e) => getFile("A11", e.target.files)}
                   />
                   K
                 </label>
-                <label className="fileupload">
+                <label className="fileupload"  style={{
+                    backgroundColor: uploads.indexOf("A12") !== -1 ? "rgba(236, 70, 92, 0.8)" : "",
+                  }}> 
                   <input
                     type="file"
                     name="file"
-                    onChange={(e) => getFile("A12",e.target.files)}
+                    onChange={(e) => getFile("A12", e.target.files)}
                   />
                   L
                 </label>
-               
               </div>
               <div className="defaultbtn">
-                <button onMouseDown={() => setSounds(defaultSounds)}>default</button>
-                </div>
+                <button onMouseDown={setDefault}>
+                  default
+                </button>
+              </div>
             </div>
           </div>
         </React.Fragment>,
